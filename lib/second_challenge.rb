56e1ae8dd4b81e2dd480e56c2e078cfdb5a56fe8
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-25 00:49:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch{}
-      :matriarch{}
-      :hero{}
+      :patriarch{},
+      :matriarch{},
+      :hero{},
       :hero_friends[]
       #your key/value pairs here!
       },
     :capulet => {
-      :patriarch{}#your key/value pairs here!
-      :matriarch{}
-      :heroine{}
+      :patriarch{},#your key/value pairs here!
+      :matriarch{},
+      :heroine{},
       :heroine_friends[]
       }
     }
